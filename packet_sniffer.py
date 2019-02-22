993909c8bd5c2a5a1c6ef343e3915399393cf53e
Shubham Saini <shubham6405@gmail.com>
2019-02-22 20:17:33 +0530

    Added argument for interface

--- packet_sniffer.py
+++ packet_sniffer.py
@@ -1,9 +1,18 @@
 #!/usr/bin/env python
 import scapy.all as scapy
+import argparse
 from scapy_http import http
 
 def sniff(interface):
     scapy.sniff(iface=interface, store=False, prn=process_packet)
+
+def get_arguments():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-i", "--interface", dest="target", help="Specify interface")
+    options = parser.parse_args()
+    if not options.target:
+        parser.error("[-] Please specify a interface, Use --help for more info.")
+    return options
 
 def process_packet(packet):
     if packet.haslayer(http.HTTPRequest):
@@ -17,4 +26,5 @@ def process_packet(packet):
                 if keyword in load:
                     print("\n\nPossible username/password >>" + load + "\n\n")
                     break
-sniff("wlo1")
+options = get_arguments()
+sniff(options.target)
